2f19e9adb466d220d79c39927e2d856b54457f4c
Laksamadi Guko <elru.lakusa@gmail.com>
2019-04-20 11:13:46 +0700

    Update geb.js

--- geb.js
+++ geb.js
@@ -4,7 +4,7 @@
 // @updateURL    https://raw.githubusercontent.com/laksa19/GEB/master/geb.js
 // @downloadURL  https://raw.githubusercontent.com/laksa19/GEB/master/geb.js
 // @iconURL      https://raw.githubusercontent.com/laksa19/GEB/master/geb-icon.png
-// @version      0.0.7
+// @version      0.0.8
 // @author       Laksamadi Guko
 // @description  Hide Elements and Gif images
 // @match        *://*/*
@@ -82,7 +82,7 @@ var domArr = ["",
 // hide Gif & elements
 if ((domArr.indexOf(domain)) > 0){
 // Gif images
-    //HideGif()
+    HideGif()
 // elements
 // indoxxi
     HideId("home-bnner-content")
